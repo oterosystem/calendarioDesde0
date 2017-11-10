3389ec86de5ca535ba940ca78e00f25253e9ef4f
luis otero <scholalm08@gmail.com>
2017-11-10 16:40:27 +0100

    calendario inicio

---
+++ FechaYHora.java
@@ -0,0 +1,44 @@
+
+/**
+ * Write a description of class FechaYHora here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FechaYHora
+{
+    // instance variables - replace the example below with your own
+    private int dia;
+    private int mes;
+    private int ano;
+    private int horas;
+    private int minutos;
+
+    /**
+     * Constructor FechaYHora
+     */
+    public FechaYHora()
+    {
+        // initialise instance variables
+        dia = 01;
+        mes = 01;
+        ano = 2001;
+        horas = 00;
+        minutos = 00;
+    }
+
+    /**
+     * metodo sin parametros que contiene una fecha dada.
+     */
+    public String getFechaYHora(String fechahora)
+    {
+        // put your code here
+         String fechahora2;
+        dia = 07;
+        mes = 11;
+        ano = 2017;
+        horas = 8;
+        minutos = 35;
+        return fechahora;
+    }
+}
